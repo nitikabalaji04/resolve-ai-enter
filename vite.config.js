ffb0a415311827b62f3da21ad61defa4a5c38722
enter-pro <github@enter.pro>
2026-09-17 14:06:45 +0000

    Add vite-plugin-enter-dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { enterDevPlugin } from "vite-plugin-enter-dev";
+import { enterProdPlugin } from "vite-plugin-enter-dev";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ...enterProdPlugin(), ...enterDevPlugin({
+    react: false
+  })]
+});
